8ec5c98bc7399fc759601a12a7ce6259fcf4961e
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2023-01-26 17:28:06 +0530

    Signup Screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+// import Login from './components/authentication/Login';
+import SignUp from './components/authentication/SignUp';
+
+function App() {
+  return (
+    <Fragment>
+      <SignUp />
+    </Fragment>
+  );
+}
+
+export default App;
